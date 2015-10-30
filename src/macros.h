3a572b068c2139e5b63056bde137bd7f252d586e
shorC <3130102666@zju.edu.cn>
2015-10-30 19:46:47 +0800

    add macro.h

---
+++ src/macros.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// MINISQL_MACROS
+
+#define	MINISQL_OK			(0)				// query success
+#define	MINISQL_ETYPE		(-1)			// Type error
+#define	MINISQL_EIO			(-2)			// Table not found 
+#define	MINISQL_ECONSTRAINT	(-3)			// Constraints error 
