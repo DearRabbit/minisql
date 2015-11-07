1a412e0f1e722b32309420c86035c8f882c6f31a
DearRabbit <peachandgrape@gmail.com>
2015-11-07 20:15:39 +0800

    sql

--- example/sql.sql
+++ example/sql.sql
@@ -1,2 +1,7 @@
-create table a (id int);
-create 
+insert into b values('name1', 'unique1', 10, 'M');
+insert into b values('name2', 'unique2', 10, 'M');
+insert into b values('name3', 'unique3', 10, 'M');
+insert into b values('name4', 'unique4', 10, 'M');
+insert into b values('name5', 'unique5', 10, 'M');
+insert into b values('name6', 'unique6', 10, 'M');
+insert into b values('name0', 'unique0', 9, 'M');
